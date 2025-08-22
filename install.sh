ac991253268b1e287b40a574db2ba62f3cb13ee6
Victor Faure <faurevic@gmail.com>
2025-08-22 12:09:41 +0200

    Better install file - Default config home to $XDG_CONFIG_HOME else $HOME/.config - Interactive deletion - Verbose commands - Smaller overall

--- install.sh
+++ install.sh
@@ -7,21 +7,19 @@ if [[ $EUID -eq 0 ]]; then
    exit 1
 fi
 
-DOTFILES_DIR="$(pwd)"
-CONFIG_DIR="~/.config"
+CURRENT_DIR="$(pwd)"
+CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
 
 rm -f $HOME/.zshenv
-ln -sf $DOTFILES_DIR/.zshenv $HOME/.zshenv
+ln -sfv $CURRENT_DIR/.zshenv $HOME/.zshenv
 
 rm -f $HOME/.zshrc
-ln -sf $DOTFILES_DIR/.zshrc $HOME/.zshrc
+ln -sfv $CURRENT_DIR/.zshrc $HOME/.zshrc
 
-for d in $DOTFILES_DIR/*/; do
+for d in $CURRENT_DIR/*/; do
    IFS='/' read -ra DIRS <<< "$d"
    SRC_DIR=${d%/}
-   DST_DIR="$HOME/.config/"
-   DEL_DIR="$DST_DIR${DIRS[-1]}"
-   echo "SRC_DIR=$SRC_DIR DST_DIR=$DST_DIR DEL_DIR=$DEL_DIR"
-   rm -rf $DEL_DIR 
-   ln -sf $SRC_DIR $DST_DIR
+   DST_DIR=$CONFIG_HOME/${DIRS[-1]}
+   rm -irv $DST_DIR
+   ln -sfv $SRC_DIR $DST_DIR
 done
